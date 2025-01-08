f704e5415ea6b7f50bd121ad187e4f66c80934d0
ThanoojKhan <thanu1495@gmail.com>
2025-01-08 21:29:26 +0530

    refresh token bug

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,36 +5,30 @@ const env = require('../util/validateEnv');
 const jwt = require('jsonwebtoken');
 
 exports.authMiddleware = catchAsync(async (req, res, next) => {
-
-     // Skip the authorization check for the registration endpoint
-     if (req.url === '/api/admin/auth/registerAdmin') {
+    const publicRoutes = ['/api/admin/auth/registerAdmin', '/api/admin/auth/login'];
+    if (publicRoutes.includes(req.path)) {
         return next();
     }
 
-    // Get the access token from the authorization header
     if (!req.headers.authorization) {
+        throw new AppError({ name: 'Unauthorized', statusCode: 401, message: 'Access token missing or invalid' });
+    }
+
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedAccessToken = jwt.verify(token, env.ACCESS_TOKEN_SECRET);
+
+        const admin = await adminModel.findById(decodedAccessToken.adminId);
+        if (!admin) {
+            throw new AppError({ name: 'Unauthorized', statusCode: 401, message: 'Admin not found' });
+        }
+
+        req.admin = admin;
+        next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            throw new AppError({ name: 'Unauthorized', statusCode: 401, message: 'Access token expired' });
+        }
         throw new AppError({ name: 'Unauthorized', statusCode: 401, message: 'Invalid access token' });
     }
-
-    // Get refresh token
-    const refreshToken = req.cookies.refreshToken;
-    if (!refreshToken) {
-        throw new AppError({ name: 'Unauthorized', statusCode: 401, message: 'Invalid refresh token' });
-    }
-
-    // Verify tokens
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedAccessToken = jwt.verify(token, env.ACCESS_TOKEN_SECRET);
-    const decodedRefreshToken = jwt.verify(refreshToken, env.REFRESH_TOKEN_SECRET);
-
-    const adminId = decodedRefreshToken.adminId === decodedAccessToken.adminId ? decodedRefreshToken.adminId : null;
-
-    // Find admin
-    const admin = await adminModel.findById(adminId);
-    if (!admin) {
-        throw new AppError({ name: 'Unauthorized', statusCode: 401, message: 'Invalid token' });
-    }
-
-    req.admin = admin;
-    next();
 });
